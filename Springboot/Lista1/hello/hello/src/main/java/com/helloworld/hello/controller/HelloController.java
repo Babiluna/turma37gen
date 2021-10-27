d32bb9fffd58d4fc468ff7b95094fe6600b17b3e
babiluna <barbara_luna01@hotmail.com>
2021-10-27 19:40:47 -0300

    Hello world

---
+++ Springboot/Lista1/hello/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class HelloController {
+
+	@GetMapping ("/hello")
+	public String hello() {
+		return "Hello World";
+		
+	}
+	@GetMapping ("/atividade1")
+	public String hello2() {
+		return "Habilidade: Persistência. \nMentalidade: Atenção aos detalhes";
+}
+	@GetMapping ("/atividade2")
+	public String hello3() {
+		return "Objetivo de aprendizagem: Aprender a utilizar o Spring Boot.";
+}
+}
