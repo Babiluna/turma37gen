1b5b03e6945fc94918e6bbb85fe475e2b18b98ea
babiluna <barbara_luna01@hotmail.com>
2021-11-22 13:34:57 -0300

    update Controller, Swagger, Usuario e UsuarioService

--- Springboot/blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ Springboot/blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,31 +1,72 @@
 package org.generation.blogPessoal.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
-@Table(name = "tb_usuario")
+@Table(name =  "tb_usuario")
 public class Usuario {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
-	@Size(min = 2, max = 100)
+	@NotNull(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100,
+	message = "O atributo nome deve ter no mínimo 05 e no máximo 100 caracteres")
 	private String nome;
 	
-	@NotNull
-	@Size(min = 5, max = 100)
+	@ApiModelProperty(example = "email@email.com.br")
+	@NotNull(message = "O atributo usuário é obrigatório")
+	@NotBlank(message = "O atributo usuário não pode ser vazio")
+	@Email(message = "O atributo usuário deve ser um email")
 	private String usuario;
 	
-	@NotNull
-	@Size(min = 5, max = 100)
+	@NotNull(message = "O atributo senha é obrigatório")
+	@Size(min = 7, message = "O atributo senha deve ter no mínimo 7 caracteres")
 	private String senha;
+	
+	@Size(min = 3, message = "O atributo senha deve ter no mínimo 3 caracteres")
+	private String foto;
+	
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataNascimento;
+	
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+		private List <Postagem> postagem;
+	
+
+	public Usuario(long id,String nome, String usuario, String senha, LocalDate dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public Usuario() {
+		
+	}
 
 	public long getId() {
 		return id;
@@ -59,7 +100,28 @@ public class Usuario {
 		this.senha = senha;
 	}
 	
-	
-	
+	public String getFoto() {
+		return foto;
+	}
 
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public LocalDate getDataNascimento() {
+		return this.dataNascimento;
+	}
+	
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public List<Postagem> getPostagem() {
+		return this.postagem;
+	}
+	
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
 }
--- Springboot/blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ Springboot/blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -18,17 +18,20 @@ public class UsuarioService {
 	private UsuarioRepository repository;
 	
 	public Usuario CadastrarUsuario(Usuario usuario) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String senhaEncoder = encoder.encode(usuario.getSenha());
+		
 		usuario.setSenha(senhaEncoder);
 		
 			return repository.save(usuario);
 	}
 	
 	public Optional<UserLogin> Logar(Optional<UserLogin>user) {
+		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
 		
 		if(usuario.isPresent()) {
 			if(encoder.matches(user.get().getSenha(),usuario.get().getSenha())) {
@@ -39,6 +42,7 @@ public class UsuarioService {
 				
 				user.get().setToken(authHeader);
 				user.get().setName(usuario.get().getNome());
+				
 				
 				return user;
 				
